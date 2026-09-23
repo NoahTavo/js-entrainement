e3f7591ee90000b78f69bd4c5e10086d8f88918b
Noah Tavo <noah.tavo@dynedoc.fr>
2026-09-23 15:41:30 +0200

    refactor: change isobstacle to boolean

--- js/Cell.js
+++ js/Cell.js
@@ -1,47 +1,55 @@
 class Cell {
     constructor(x, y, obstacleRate) {
-        this.x = x;
-        this.y = y;
-        this.obstacleRate = obstacleRate;
-        this.content = this.selectContent();
+        this._x = x;
+        this._y = y;
+        this._obstacleRate = obstacleRate;
+        this._content = this.selectContent();
     }
 
-    getX() {
-        return this.x;
+    get x() {
+        return this._x;
     }
 
-    getY() {
-        return this.y;
+    get y() {
+        return this._y;
+    }
+
+    get content() {
+        return this._content;
+    }
+
+    set content(value) {
+        this._content = value;
     }
 
     selectContent() {
         const random = Math.random();
 
-        if (random < this.obstacleRate) {
+        if (random < this._obstacleRate) {
             return "obstacle";
         }
 
         return "empty";
     }
 
-    isObstacle() {
-        return this.content === "obstacle";
+    get isObstacle() {
+        return this._content === "obstacle";
     }
 
-    isEmpty() {
-        return this.content === "empty";
+    get isEmpty() {
+        return this._content === "empty";
     }
 
     placeWeapon(weapon) {
-        this.content = weapon;
+        this._content = weapon;
     }
 
     placeCharacter(character) {
-        this.content = character;
+        this._content = character;
     }
 
     clear() {
-        this.content = "empty";
+        this._content = "empty";
     }
 
     render() {
@@ -51,7 +59,7 @@ class Cell {
         element.dataset.y = this.y;
         element.dataset.testid = `map-cell-${this.x}-${this.y}`;
 
-        if (this.isObstacle()) {
+        if (this.isObstacle) {
             element.classList.add("map__cell--obstacle");
         }
 
